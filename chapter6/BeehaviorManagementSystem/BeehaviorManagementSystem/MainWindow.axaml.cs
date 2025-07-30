2e905d4b3ce40f3fe753d25d1e0bcb001c47c800
Fabio William Conceição <messhias@gmail.com>
2025-07-30 09:05:22 +0100

    UI completed.

---
+++ chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/MainWindow.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace BeehaviorManagementSystem;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void WorkShift_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void AssignJob_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
