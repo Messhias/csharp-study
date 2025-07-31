e9b0f5fcc32c4a6dd9c5c9d9c887bd3966a0db63
Fabio William Conceição <messhias@gmail.com>
2025-07-31 20:02:13 +0100

    updates.

--- chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/HoneyManufacturer.cs
+++ chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/HoneyManufacturer.cs
@@ -1,12 +1,16 @@
 namespace BeehaviorManagementSystem;
 
 public class HoneyManufacturer : Bee
-
 {
     public override float CostPerShift => 1.7f;
+    public const float NECTAR_COLLECTED_PER_SHIFT = 33.15f;
 
     public HoneyManufacturer() : base("Honey Manufacturer")
     {
-         
+    }
+
+    protected override void DoJob()
+    {
+        HoneyVault.CollectNectar(NECTAR_COLLECTED_PER_SHIFT);
     }
 }
