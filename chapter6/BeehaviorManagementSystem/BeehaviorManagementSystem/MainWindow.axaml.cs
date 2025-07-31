fb1a52ab9261c786884d833f767dfe970215962b
Fabio William Conceição <messhias@gmail.com>
2025-07-31 21:00:13 +0100

    Make the game realtime.

--- chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/MainWindow.axaml.cs
+++ chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/MainWindow.axaml.cs
@@ -1,16 +1,27 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Threading;
 
 namespace BeehaviorManagementSystem;
 
 public partial class MainWindow : Window
 {
     private readonly Queen _queen = new();
+    private readonly DispatcherTimer _timer = new  DispatcherTimer();
 
     public MainWindow()
     {
         InitializeComponent();
         StatusReport.Text = _queen.StatusReport;
+        _timer.Tick += Timer_Tick;
+        _timer.Interval = TimeSpan.FromSeconds(0.5);
+        _timer.Start();
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        WorkShift_Click(this, new RoutedEventArgs());
     }
 
     private void WorkShift_Click(object? sender, RoutedEventArgs e)
