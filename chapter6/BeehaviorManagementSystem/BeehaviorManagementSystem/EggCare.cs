16edb02fd05b5a79b9267b97465adcacb3b55cea
Fabio William Conceição <messhias@gmail.com>
2025-07-31 20:20:59 +0100

    Eggcare class updates.

--- chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/EggCare.cs
+++ chapter6/BeehaviorManagementSystem/BeehaviorManagementSystem/EggCare.cs
@@ -2,9 +2,18 @@ namespace BeehaviorManagementSystem;
 
 public class EggCare : Bee
 {
+    public const float CARE_PROGRESS_PER_SHIFT = 0.15f;
     public override float CostPerShift => 1.35f;
 
-    public EggCare() : base("Egg Care")
+    private Queen _queen;
+
+    public EggCare(Queen queen) : base("Egg Care")
     {
+        _queen = queen;
+    }
+
+    protected override void DoJob()
+    {
+        _queen.CareForEggs(CARE_PROGRESS_PER_SHIFT);
     }
 }
