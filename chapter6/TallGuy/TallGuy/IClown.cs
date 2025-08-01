4c34a63e6db6a0df557579f9a2211b735f6091d5
Fabio William Conceição <messhias@gmail.com>
2025-08-02 00:44:35 +0100

    Clown completed.

---
+++ chapter6/TallGuy/TallGuy/IClown.cs
@@ -0,0 +1,7 @@
+public interface IClown
+{
+    public string FunnyThingIHave { get; }
+    
+    void Honk();
+    void TalkAboutYourself();
+}
---
+++ chapter6/TallGuy/TallGuy/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+IClown tall = new TallGuy()
+{
+    Height = 90,
+    Name = "Jon",
+};
+
+tall.TalkAboutYourself();
+Console.Write($"The tall guy has {tall.FunnyThingIHave}");
+tall.Honk();
+
+class TallGuy : IClown
+{
+    public string Name;
+    public int Height;
+
+    public string FunnyThingIHave => "Big shoes";
+
+    public void Honk()
+    {
+        Console.WriteLine("Honk honk!");
+    }
+
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+    }
+}
