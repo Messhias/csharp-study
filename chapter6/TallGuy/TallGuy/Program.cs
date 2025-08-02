968785c8c63771169370e953c9ec1f251a025cd8
Fabio William Conceição <messhias@gmail.com>
2025-08-02 17:28:14 +0100

    refactor started.

--- chapter6/TallGuy/TallGuy/Program.cs
+++ chapter6/TallGuy/TallGuy/Program.cs
@@ -9,21 +9,3 @@ IClown tall = new TallGuy()
 tall.TalkAboutYourself();
 Console.Write($"The tall guy has {tall.FunnyThingIHave}");
 tall.Honk();
-
-class TallGuy : IClown
-{
-    public string Name;
-    public int Height;
-
-    public string FunnyThingIHave => "Big shoes";
-
-    public void Honk()
-    {
-        Console.WriteLine("Honk honk!");
-    }
-
-    public void TalkAboutYourself()
-    {
-        Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
-    }
-}
---
+++ chapter6/TallGuy/TallGuy/TallGuy.cs
@@ -0,0 +1,18 @@
+
+class TallGuy : IClown
+{
+    public string Name;
+    public int Height;
+
+    public string FunnyThingIHave => "Big shoes";
+
+    public void Honk()
+    {
+        Console.WriteLine("Honk honk!");
+    }
+
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+    }
+}
