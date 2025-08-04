537be415f56199f1cee9e4c04175a2c39b4efb4f
Fabio William Conceição <messhias@gmail.com>
2025-08-04 20:12:30 +0100

    Shoes class list completed.

--- chapter8/ShoesShop/ShoesShop/Program.cs
+++ chapter8/ShoesShop/ShoesShop/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ShoeCloset shoeCloset = new ShoeCloset();
+
+while (true)
+{
+    shoeCloset.PrintShoes();
+    Console.WriteLine("press 'a' to add or 'r' to remove shoes.");
+    char key = Console.ReadKey().KeyChar;
+
+    switch (key)
+    {
+        case 'a':
+        case 'A':
+            shoeCloset.AddShoe();
+            break;
+        case 'r':
+        case 'R':
+            shoeCloset.RemoveShoe();
+            break;
+    }
+}
--- chapter8/ShoesShop/ShoesShop/ShoeCloset.cs
+++ chapter8/ShoesShop/ShoesShop/ShoeCloset.cs
@@ -1,7 +1,52 @@
 public class ShoeCloset
 {
-    private List<Shoe>  shoes;
-    
-    void AddShoe(){}
-    void RemoveShoe(){}
+    private readonly List<Shoe> _shoes =  new List<Shoe>();
+
+    public void PrintShoes()
+    {
+        if (_shoes.Count == 0)
+        {
+            Console.WriteLine("\nThe shoe closet is empty.");
+        }
+        else
+        {
+            Console.WriteLine("\nThe shoe closet contains:");
+            int i = 0;
+            foreach (Shoe shoe in _shoes)
+            {
+                Console.WriteLine($"Shoe {i++}: {shoe.Description}");
+            }
+        }
+    }
+
+    public void AddShoe()
+    {
+        Console.WriteLine("Add a shoe to the shoe closet.");
+        for (int i = 0; i < 6; i++)
+        {
+            Style styleName = (Style)i;
+            Console.WriteLine($"Press {i} to add a {styleName}");
+        }
+        
+        Console.WriteLine("Enter a style:");
+
+        if (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out int style)) return;
+        
+        
+        Console.WriteLine("Enter the color");
+        string color = Console.ReadLine();
+        Shoe shoe = new Shoe((Style)style, color);
+        _shoes.Add(shoe);
+    }
+
+    public void RemoveShoe()
+    {
+        Console.WriteLine("Enter shoe number to remove");
+        if (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out int shoeNumber) ||
+            (shoeNumber < 1 || shoeNumber > _shoes.Count)) return;
+        
+        
+        Console.WriteLine($"\nRemoving {_shoes[shoeNumber - 1].Description}");
+        _shoes.RemoveAt(shoeNumber - 1);
+    }
 }
