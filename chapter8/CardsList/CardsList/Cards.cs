abac132f9f532a6381e434fd21f5d59e7a556c5c
Fabio William Conceição <messhias@gmail.com>
2025-08-04 23:34:33 +0100

    WIP

---
+++ chapter8/CardsList/CardsList/Cards.cs
@@ -0,0 +1,14 @@
+
+public class Cards
+{
+    public Values Values { get; private set; }
+    public Suits Suit { get; private set; }
+
+    public string Name => $"{Values} of  {Suit}";
+
+    public Cards(Values values, Suits suit)
+    {
+        Values = values;
+        Suit = suit;
+    }
+}
