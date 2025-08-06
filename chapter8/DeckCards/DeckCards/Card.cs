9ea8871e8c7f86fb416128292b3cab732639dbf1
Fabio William Conceição <messhias@gmail.com>
2025-08-06 09:00:46 +0100

    WIP

--- chapter8/DeckCards/DeckCards/Card.cs
+++ chapter8/DeckCards/DeckCards/Card.cs
@@ -1,12 +1,12 @@
 namespace DeckCards;
-public class Cards
+public class Card
 {
     public Values Values { get; private set; }
     public Suits Suit { get; private set; }
 
     public string Name => $"{Values} of  {Suit}";
 
-    public Cards(Values values, Suits suit)
+    public Card(Values values, Suits suit)
     {
         Values = values;
         Suit = suit;
--- chapter8/DeckCards/DeckCards/MainWindow.axaml.cs
+++ chapter8/DeckCards/DeckCards/MainWindow.axaml.cs
@@ -9,4 +9,24 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void MoveCard(bool leftToRight)
+    {
+        if ((Resources["rightDeck"] is not Deck rightdeck) || (Resources["leftDeck"] is not Deck leftdeck)) return;
+        
+        if (leftToRight)
+        {
+            if (leftDeckListBox.SelectedItems is not Card card) return;
+            
+            leftdeck.Remove(card);
+            rightdeck.Add(card);
+        }
+        else
+        {
+            if (rightDeckListBox.SelectedItem is not Card card) return;
+            
+            rightdeck.Remove(card);
+            leftdeck.Add(card);
+        }
+    }
 }
