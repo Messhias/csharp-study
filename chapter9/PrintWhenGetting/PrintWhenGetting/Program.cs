d885016d239e2a69747e7f0550ef9a70c4e1ec97
Fabio William Conceição <messhias@gmail.com>
2025-08-09 10:52:49 +0100

    Learned LINQ immediate expression.

---
+++ chapter9/PrintWhenGetting/PrintWhenGetting/Program.cs
@@ -0,0 +1,34 @@
+﻿var listOfObjects = new List<PrintWhenGetting>();
+
+for (var i = 0; i < 5; i++)
+{
+    listOfObjects.Add(new PrintWhenGetting { InstanceNumber = i });
+}
+
+Console.WriteLine("Set up the query.");
+
+var result = from o in listOfObjects
+    select o.InstanceNumber;
+
+var immediate = result.ToList();
+
+Console.WriteLine("Run the foreach");
+foreach (var i in immediate)
+{
+    Console.WriteLine($"Writing #{i}");
+}
+
+class PrintWhenGetting
+{
+    private readonly int _instanceNumber;
+
+    public int InstanceNumber
+    {
+        init => _instanceNumber = value;
+        get
+        {
+            Console.WriteLine($"Getting #{_instanceNumber}");
+            return _instanceNumber;
+        }
+    }
+}
