beec5629f594798db5e1f04b5bc3613262786754
Fabio William Conceição <messhias@gmail.com>
2025-08-11 22:24:13 +0100

    The power of two completed.

---
+++ chapter9/PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+var power = new PowerOfTwo();
+
+foreach (var p in power)
+{
+    Console.Write($"{p} ");
+}
+
+internal class PowerOfTwo : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        var maximumPowerOfTwo = Math.Round(Math.Log(int.MaxValue, 2));
+
+        for (int power = 0; power <= maximumPowerOfTwo; power++)
+        {
+            yield return (int)Math.Pow(2, power);
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
