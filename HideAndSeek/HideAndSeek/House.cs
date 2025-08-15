fbad4efbd8f2b84f7b11b2f98e7e924739e5497d
Fabio William Conceição <messhias@gmail.com>
2025-08-15 23:47:35 +0100

    house unit tests finished.

--- HideAndSeek/HideAndSeek/House.cs
+++ HideAndSeek/HideAndSeek/House.cs
@@ -2,10 +2,55 @@ namespace HideAndSeek;
 
 public static class House
 {
-    private static Location Entry;
+    public static Location Entry;
 
     static House()
     {
+        Entry = new Location("Entry");
+        var hallway = new Location("Hallway");
+        var livingRoom = new Location("Living Room");
+        var kitchen = new Location("Kitchen");
+        var bathroom = new Location("Bathroom");
+        var landing = new Location("Landing");
+        var masterBedroom = new Location("Master Bedroom");
+        var masterBath = new Location("Master Bath");
+        var secondBathroom = new Location("Second Bathroom");
+        var kidsRoom = new Location("Kids Room");
+        var nursery = new Location("Nursery");
+        var pantry = new Location("Pantry");
+        var attic = new Location("Attic");
+        var garage = new Location("Garage");
+
+        EntrySetup(hallway, garage);
+        HallwaySetup(hallway, kitchen, bathroom, livingRoom, landing);
+        LandingSetup(landing, masterBedroom, secondBathroom, nursery, pantry, kidsRoom, attic);
         
+        masterBedroom.AddExit(Direction.East, masterBath);
+    }
+
+    private static void LandingSetup(Location landing, Location masterBedroom, Location secondBathroom, Location nursery,
+        Location pantry, Location kidsRoom, Location attic)
+    {
+        landing.AddExit(Direction.Northwest, masterBedroom);
+        landing.AddExit(Direction.West, secondBathroom);
+        landing.AddExit(Direction.Southwest, nursery);
+        landing.AddExit(Direction.South, pantry);
+        landing.AddExit(Direction.Southeast, kidsRoom);
+        landing.AddExit(Direction.Up, attic);
+    }
+
+    private static void HallwaySetup(Location hallway, Location kitchen, Location bathroom, Location livingRoom,
+        Location landing)
+    {
+        hallway.AddExit(Direction.Northwest, kitchen);
+        hallway.AddExit(Direction.North, bathroom);
+        hallway.AddExit(Direction.South, livingRoom);
+        hallway.AddExit(Direction.Up, landing);
+    }
+
+    private static void EntrySetup(Location hallway, Location garage)
+    {
+        Entry.AddExit(Direction.East, hallway);
+        Entry.AddExit(Direction.Out, garage);
     }
 }
